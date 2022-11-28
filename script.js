95437f28fc8756bb6274ae8504e4ec4c9a4bd0cf
riand01 <riand01@gmail.com>
2022-11-28 14:00:58 +0100

    bokabanaKlart

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-// Get the modal
+// ------------------------------------------Get the modal-------------------------------------------------------
+
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
@@ -8,6 +9,8 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+//------------------------------------------PopupImage-----------------------------------------------------------
 
 document.querySelectorAll('.company-info img').forEach(image =>{
     image.onclick = () =>{
@@ -20,3 +23,29 @@ document.querySelector('.popupimage span').onclick = () =>{
     document.querySelector('.popupimage').style.display = 'none';
 }
 
+//--------------------------------------------Footer-----------------------------------------------------------
+
+// const btns = document.querySelectorAll('.btn');
+// const dropMenus = document.querySelectorAll('.drop-menu');
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         removeActive();
+//         btn.classList.add('active');
+//         document.querySelector(btn.dataset.target).classList.add('active');
+//     })
+// })
+
+// const removeActive = () => {
+//     btns.forEach(btn => btn.classList.remove('active'));
+//     dropMenus.forEach(dropmenu => dropmenu.classList.remove('active'));
+// }
+
+// window.onclick = (e) => {
+//     if (!e.target.matches('.btn')) {
+//         removeActive()
+//     }
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
